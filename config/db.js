5ae16de12e46985487326025a6c98cf61276b824
kru1980 <kru71101@gmail.com>
2019-09-22 07:05:08 +0500

    220919

--- config/db.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    mongoURI:
-      "mongodb+srv://vatafaker:kru1980@cluster0-crf9o.mongodb.net/test?retryWrites=true&w=majority"
-  };
-} else {
-  module.exports = { mongoURI: "mongodb://localhost/vidjot-dev" };
-}
